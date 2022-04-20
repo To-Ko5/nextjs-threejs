2eed3f455b52835721cbc6a1e9cb39df2292f99b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-04-20 22:50:43 +0900

    Add: threejs renderer

--- pages/one.tsx
+++ pages/one.tsx
@@ -1,17 +1,26 @@
+import { useEffect } from 'react'
 import * as THREE from 'three'
+
 const one = () => {
-  let scene, camera
+  useEffect(() => {
+    let scene, camera, renderer
 
-  // シーンの追加
-  scene = new THREE.Scene()
+    // シーンの追加
+    scene = new THREE.Scene()
 
-  // カメラの追加
-  camera = new THREE.PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  )
+    // カメラの追加
+    camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+
+    // レンダラーの追加
+    renderer = new THREE.WebGLRenderer({ alpha: true })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+  }, [])
 
   return (
     <>
