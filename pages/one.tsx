951887db744a64f6e9fd81b9934a77a6c28d8f9a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-04-20 22:42:13 +0900

    Add: threejs camera

--- pages/one.tsx
+++ pages/one.tsx
@@ -1,7 +1,17 @@
 import * as THREE from 'three'
 const one = () => {
-  let scene
+  let scene, camera
+
+  // シーンの追加
   scene = new THREE.Scene()
+
+  // カメラの追加
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
 
   return (
     <>
