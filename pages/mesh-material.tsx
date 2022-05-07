dfa6830d139a3d61cc817ea816505d1cfcf360df
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-05-07 21:43:43 +0900

    Add: meshMaterial Object

--- pages/mesh-material.tsx
+++ pages/mesh-material.tsx
@@ -1,4 +1,83 @@
+import { useEffect } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import GUI from 'lil-gui'
+
 const meshMaterial = () => {
+  useEffect(() => {
+    let scene: THREE.Scene | THREE.Object3D<THREE.Event>,
+      camera: THREE.PerspectiveCamera | THREE.Camera | any,
+      renderer: THREE.WebGLRenderer,
+      pointLight,
+      controls,
+      sphere,
+      plane,
+      octahedron
+
+    window.addEventListener('load', init)
+
+    function init() {
+      // シーン
+      scene = new THREE.Scene()
+
+      // カメラ
+      camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        100
+      )
+      camera.position.set(1, 1, 2)
+
+      // レンダラー
+      renderer = new THREE.WebGLRenderer()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+      document.body.appendChild(renderer.domElement)
+
+      /**
+       * マテリアルセクション
+       */
+
+      // ジオメトリ
+      const sphereGeometry = new THREE.SphereGeometry(0.5, 16, 16)
+      const planeGeometry = new THREE.PlaneGeometry(1, 1)
+      const octahedronGeometry = new THREE.OctahedronGeometry(0.5)
+
+      // マテリアル
+      const material = new THREE.MeshBasicMaterial()
+
+      // メッシュ化
+      sphere = new THREE.Mesh(sphereGeometry, material)
+      plane = new THREE.Mesh(planeGeometry, material)
+      octahedron = new THREE.Mesh(octahedronGeometry, material)
+
+      sphere.position.x = -1.5
+      octahedron.position.x = 1.5
+      scene.add(sphere, plane, octahedron)
+
+      // マウス操作
+      const controls = new OrbitControls(camera, renderer.domElement)
+
+      window.addEventListener('resize', onWindowResize)
+
+      animate()
+    }
+
+    function animate() {
+      // レンダリング
+      renderer.render(scene, camera)
+      requestAnimationFrame(animate)
+    }
+
+    // ブラウザのリサイズに対応
+    function onWindowResize() {
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+    }
+  })
+
   return <></>
 }
 
