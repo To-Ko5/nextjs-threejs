3e1c00dd6d49e92873a908d2b8d9e6e13134bdb3
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-05-08 21:40:37 +0900

    Add: AmbientLight

--- pages/mesh-material.tsx
+++ pages/mesh-material.tsx
@@ -56,22 +56,31 @@ const meshMaterial = () => {
       )
 
       // マテリアル
-      // const material = new THREE.MeshBasicMaterial({
-      //   color: 'red',
-      //   map: textures
-      // })
-      // // material.wireframe = true
+      const material = new THREE.MeshBasicMaterial({
+        color: 'red',
+        map: textures
+      })
+      // material.wireframe = true
 
-      // // 裏側を描写する
-      // material.side = THREE.DoubleSide
-      // // 透過する
-      // material.opacity = 0.5
-      // material.transparent = true
+      // 裏側を描写する
+      material.side = THREE.DoubleSide
+      // 透過する
+      material.opacity = 0.5
+      material.transparent = true
 
       // マテリアル（MeshNormalMaterial）
-      const material = new THREE.MeshNormalMaterial()
-      material.flatShading = true
-      material.side = THREE.DoubleSide
+      // const material = new THREE.MeshNormalMaterial()
+      // material.flatShading = true
+      // material.side = THREE.DoubleSide
+
+      // 明かりを追加
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+      scene.add(ambientLight)
+      const pointLight = new THREE.PointLight(0xffffff, 1)
+      pointLight.position.set(1, 2, 3)
+      scene.add(pointLight)
+      const pointLightHelper = new THREE.PointLightHelper(pointLight, 1)
+      scene.add(pointLightHelper)
 
       // メッシュ化
       sphere = new THREE.Mesh(sphereGeometry, material)
@@ -105,7 +114,7 @@ const meshMaterial = () => {
       octahedron.rotation.y = elapsedTime
 
       // レンダリング
-      renderer.setClearColor(0x000000, 0)
+      renderer.setClearColor(0x000000, 1)
       renderer.render(scene, camera)
       requestAnimationFrame(animate)
     }
