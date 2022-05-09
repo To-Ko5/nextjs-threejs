e0045a11bf0be70874bcdb5634317eb460125e3f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-05-09 22:46:44 +0900

    Add: particle page Three.js

--- pages/particle.tsx
+++ pages/particle.tsx
@@ -1,5 +1,66 @@
+import { useEffect } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import GUI from 'lil-gui'
+
 const particle = () => {
-  return <div>particle</div>
+  useEffect(() => {
+    // UIデバッグ
+    const gui = new GUI()
+
+    // サイズ
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+
+    // シーン
+    const scene = new THREE.Scene()
+
+    // カメラ
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.1,
+      100
+    )
+    camera.position.set(1, 1, 2)
+
+    // レンダラー
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+    document.body.appendChild(renderer.domElement)
+
+    // マウス操作
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
+
+    window.addEventListener('resize', onWindowResize)
+
+    const clock = new THREE.Clock()
+
+    function animate() {
+      const elapsedTime = clock.getElapsedTime()
+
+      controls.update()
+
+      // レンダリング
+      renderer.render(scene, camera)
+      requestAnimationFrame(animate)
+    }
+
+    // ブラウザのリサイズに対応
+    function onWindowResize() {
+      renderer.setSize(sizes.width, sizes.height)
+      camera.aspect = sizes.width / sizes.height
+      camera.updateProjectionMatrix()
+    }
+
+    animate()
+  }, [])
+
+  return <></>
 }
 
 export default particle
