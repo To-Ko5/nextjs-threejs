3639ef9ba5ea23de28390f46f62dfa286a4fb7b8
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-05-16 22:23:27 +0900

    particleにanimationを追加

--- pages/particle.tsx
+++ pages/particle.tsx
@@ -88,6 +88,16 @@ const particle = () => {
 
       controls.update()
 
+      for (let i = 0; i < count; i++) {
+        const i3 = i * 3
+
+        const x = particlesGeometry.attributes.position.array[i3 + 0]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+          elapsedTime + x
+        )
+      }
+      particlesGeometry.attributes.position.needsUpdate = true
+
       // レンダリング
       renderer.render(scene, camera)
       requestAnimationFrame(animate)
