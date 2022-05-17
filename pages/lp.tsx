d2e3a19be4fd3f81b743323de262949bc7d4fc13
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-05-17 22:47:08 +0900

    lp pageを追加

---
+++ pages/lp.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import GUI from 'lil-gui'
+
+const lp = () => {
+  return <></>
+}
+
+export default lp
