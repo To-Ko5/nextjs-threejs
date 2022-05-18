2d26d6d57a132631ce416c4c782a6ee4970c1c93
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-05-18 22:38:12 +0900

    LP pageにHeadを追加

--- pages/lp.tsx
+++ pages/lp.tsx
@@ -1,10 +1,18 @@
+import Head from 'next/head'
 import { useEffect } from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import GUI from 'lil-gui'
 
 const lp = () => {
-  return <></>
+  return (
+    <>
+      <Head>
+        <title>Three.js LP</title>
+        <meta name="description" content="Three.js demo site" />
+      </Head>
+    </>
+  )
 }
 
 export default lp
