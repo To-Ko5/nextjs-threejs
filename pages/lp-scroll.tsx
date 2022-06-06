bd4f82c3a6dceccc3de7419c94398c315345f2cb
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-06-06 17:18:56 +0900

    three.jsの設定を追加

--- pages/lp-scroll.tsx
+++ pages/lp-scroll.tsx
@@ -7,6 +7,54 @@ import GUI from 'lil-gui'
 import LpScroll from '../styles/LpScroll.module.css'
 
 const lpScroll = () => {
+  useEffect(() => {
+    // canvasを取得
+    const canvas = document.querySelector('#canvas')
+
+    // シーン
+    const scene = new THREE.Scene()
+
+    // サイズ
+    const sizes = {
+      width: innerWidth,
+      height: innerHeight
+    }
+
+    // カメラ
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.1,
+      1000
+    )
+
+    // レンダラー
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas || undefined
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+
+    // アニメーション
+    const tick = () => {
+      window.requestAnimationFrame(tick)
+      renderer.render(scene, camera)
+    }
+    tick()
+
+    // ブラウザのリサイズ処理
+    window.addEventListener('resize', () => {
+      sizes.width = window.innerWidth
+      sizes.height = window.innerHeight
+
+      camera.aspect = sizes.width / sizes.height
+      camera.updateProjectionMatrix()
+
+      renderer.setSize(sizes.width, sizes.height)
+      renderer.setPixelRatio(window.devicePixelRatio)
+    })
+  }, [])
+
   return (
     <>
       <Head>
