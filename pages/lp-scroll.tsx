20ca4716395f8f3ba1ee85f001bfe3063feefee2
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-06-06 17:15:20 +0900

    moduleのimport

--- pages/lp-scroll.tsx
+++ pages/lp-scroll.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import Head from 'next/head'
+import { useEffect } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import GUI from 'lil-gui'
+
+import LpScroll from '../styles/LpScroll.module.css'
 
 const lpScroll = () => {
-  return <div>lpScroll</div>
+  return (
+    <>
+      <Head>
+        <title>Three.js LP Scroll Page</title>
+        <meta name="description" content="Three.js LP Scroll Page" />
+      </Head>
+
+      <canvas id="canvas" className={LpScroll.canvas}></canvas>
+
+      <main className={LpScroll.main}>
+        <h1>Three.js LP Scroll Page</h1>
+
+        <section className={LpScroll.section}>
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p>Lorem ipsum dolor sit amet</p>
+        </section>
+
+        <section className={LpScroll.section}>
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p>Lorem ipsum dolor sit amet</p>
+        </section>
+
+        <section className={LpScroll.section}>
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p>Lorem ipsum dolor sit amet</p>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default lpScroll
